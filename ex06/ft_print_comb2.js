1013e8ff6afd3f87e12d6d0f2934393f97ae8f50
jeremyBdc <119399502+jeremyBdc@users.noreply.github.com>
2022-12-04 20:58:43 +0100

    ex06 - JS explained

--- ex06/ft_print_comb2.js
+++ ex06/ft_print_comb2.js
@@ -1,8 +1,10 @@
+//Fonction qui rajoute un 0 devant le chiffre il est compris en 0 et 9 compris
 function doubleDigits (digit) {
-  if (digit < 10) return '0' + digit
-  return digit.toString()
+  if (digit < 10) return '0' + digit 
+  return digit.toString() 
 }
 
+//Création des combinaisons strictement différentes
 function doubleComb() {
     for (let i = 0; i <= 99; i++) {
       for (let j = i + 1; j <= 99; j++) {
@@ -10,7 +12,6 @@ function doubleComb() {
         const combinaison = [doubleDigits(i),doubleDigits(j)].sort().join(" ");
        console.log(combinaison);
       } else {
-
       }
     }
   }
